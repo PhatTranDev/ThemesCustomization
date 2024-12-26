10a58762ba5c8a58d9e446e335616d4ff872de9b
Driss-Tran <drissdenie2407@gmail.com>
2024-12-26 10:43:49 +0700

    Custom Element js - change logic

--- assets/custom.js
+++ assets/custom.js
@@ -1,42 +1,42 @@
-class Custom extends HTMLElement {
-    index = 0;
-
+class QuantitySelector extends HTMLElement {
     constructor() {
-        super();
-        
-        this.setAttribute('custom-button-group', 'custom-button');
-        this.append(increaseButton, decreaseButton);
-
-        this.display = document.createElement('p');
-        this.display.textContent = `Current index: ${this.index}`;
-        this.append(this.display);
-
-        this.increaseButton = this.querySelector('.increase-button');
-        this.decreaseButton = this.querySelector('.decrease-button');
-        this.index = index;
-
-        this.increase();
-        this.decrease();
-        
-        console.log(index);
+      super();
+      this.quantity = 0;
+      this.attachShadow({ mode: 'open' });
+      this.render();
     }
-
-    increase() {
-        this.increaseButton.addEventListener('click', () => {
-            this.index++;
-            this.display.textContent = `Current index: ${this.index}`;
-            console.log("Increase ",this.index);
-
-        });
+  
+    render() {
+      const template = document.createElement('template');
+      template.innerHTML = `
+        <style>
+          button {
+            padding: 5px 10px;
+            margin: 0 5px;
+          }
+        </style>
+        <button id="decrease">-</button>
+        <p id="quantity">0</p>
+        <button id="increase">+</button>
+      `;
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+  
+      const decreaseBtn = this.shadowRoot.querySelector('#decrease');
+      const increaseBtn = this.shadowRoot.querySelector('#increase');
+      const quantityElement = this.shadowRoot.querySelector('#quantity');
+  
+      decreaseBtn.addEventListener('click', () => {
+        if (this.quantity > 0) {
+          this.quantity--;
+          quantityElement.textContent = this.quantity;
+        }
+      });
+  
+      increaseBtn.addEventListener('click', () => {
+        this.quantity++;
+        quantityElement.textContent = this.quantity;
+      });
     }
-
-    decrease() {
-        this.decreaseButton.addEventListener('click', () => {
-            this.index--;
-            this.display.textContent = `Current index: ${this.index}`;
-            console.log("Decrease ",this.index);
-        });
-    }
-}
-
-customElements.define('custom-button', Custom);
+  }
+  
+  customElements.define('quantity-selector', QuantitySelector);
