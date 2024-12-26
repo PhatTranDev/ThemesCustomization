f6b2594f05d6715b5ebdb68e1bc15445b9336a50
Driss-Tran <drissdenie2407@gmail.com>
2024-12-26 11:07:01 +0700

    Custom Element js - change logic 8

--- assets/custom.js
+++ assets/custom.js
@@ -1,37 +1,25 @@
-class Custom extends HTMLElement {
-    index = 0;
-
+class CustomButton extends HTMLElement {
     constructor() {
-        super();
-        
-        this.setAttribute('custom-button-group', 'custom-button');
-        this.append(increaseButton, decreaseButton);
-
-        this.display = document.createElement('p');
+      super();
+  
+      this.index = 0;
+      this.display = document.createElement('p');
+      this.display.textContent = `Current index: ${this.index}`;
+      this.append(this.display);
+  
+      this.increaseButton = this.querySelector('.increase-button');
+      this.decreaseButton = this.querySelector('.decrease-button');
+  
+      this.increaseButton.addEventListener('click', () => {
+        this.index++;
         this.display.textContent = `Current index: ${this.index}`;
-        this.append(this.display);
-
-        this.increaseButton = this.querySelector('.increase-button');
-        this.decreaseButton = this.querySelector('.decrease-button');
-
-        console.log(index);
+      });
+  
+      this.decreaseButton.addEventListener('click', () => {
+        this.index--;
+        this.display.textContent = `Current index: ${this.index}`;
+      });
     }
-
-    increase() {
-        this.increaseButton.addEventListener('click', () => {
-            this.index++;
-            this.display.textContent = `Current index: ${this.index}`;
-        });
-    }
-
-    decrease() {
-        this.decreaseButton.addEventListener('click', () => {
-            this.index--;
-            this.display.textContent = `Current index: ${this.index}`;
-            console.log("Decrease ",this.index);
-        });
-    }
-
-}
-
-customElements.define('custom-button', Custom);
+  }
+  
+  customElements.define('custom-button', CustomButton);
